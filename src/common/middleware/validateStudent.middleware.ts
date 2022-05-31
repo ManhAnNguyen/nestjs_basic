f481379b4796df983266d10e369d6a34aa03178a
ManhAnNguyen <thungantn1999@gmail.com>
2022-05-31 14:27:56 +0700

    add middleware  session

---
+++ src/common/middleware/validateStudent.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware, HttpException } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { students } from 'src/db';
+
+@Injectable()
+export class ValidateStudentMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Middleware');
+    const studentId = req.params.studentId;
+    const exsistingStudent = students.some(
+      (student) => student.id === studentId,
+    );
+    if (exsistingStudent) {
+      next();
+    } else {
+      throw new HttpException('Student not found', 400);
+    }
+  }
+}
